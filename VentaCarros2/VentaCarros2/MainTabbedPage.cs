f5930ce8b3bde0fe76933d4aa3315afb2383e25f
Yael Chavez <you400@gmail.com>
2021-07-26 19:06:17 -0500

    Agregar archivos de proyecto.

---
+++ VentaCarros2/VentaCarros2/MainTabbedPage.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace VentaCarros2
+{
+    //creamos una vista con puro C#
+    public class MainTabbedPage : TabbedPage
+    {
+        /// <summary>
+        /// asignamos el titulo principal de nuestra tabbed page
+        /// </summary>
+        public MainTabbedPage()
+        {
+            Title = "Vende mi carro";
+        }
+
+    }
+}
