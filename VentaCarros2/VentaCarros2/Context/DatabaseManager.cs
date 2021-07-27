7127205aa8cadb7dee7dae4b58a27a220f2d1c97
Yael Chavez <you400@gmail.com>
2021-07-27 18:49:22 -0500

    SQLite 2 y database manager

---
+++ VentaCarros2/VentaCarros2/Context/DatabaseManager.cs
@@ -0,0 +1,73 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using VentaCarros2.DependencyServices;
+using VentaCarros2.Models;
+using Xamarin.Forms;
+
+namespace VentaCarros2.Context
+{
+    public class DatabaseManager
+    {
+        private SQLiteConnection db;
+
+        public DatabaseManager()
+        {
+            //obtenemos la connection de la plataforma en uso
+            db = DependencyService.Get<ISQLite>().GetConnection();
+
+            if (!TableExists("Car"))
+                db.CreateTable<Car>();
+
+        }
+        /// <summary>
+        /// nos regresa la lista de coches marcados como favoritos desde la tabla Car
+        /// </summary>
+        /// <returns></returns>
+        public List<Car> GetFavoriteCars() => db.Query<Car>("Select * from car");
+
+        /// <summary>
+        /// valida si el carro no existe ya en la tabla y lo inserta
+        /// </summary>
+        /// <param name="car"></param>
+        /// <returns></returns>
+        public bool AddFavoriteCar(Car car)
+        {
+            if (db.Query<Car>($"select * from car where id = {car.Id}").Count == 0)
+            {
+                db.Insert(car);
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+
+
+        /// <summary>
+        /// Valida si la tabla exista en la base de datos
+        /// </summary>
+        /// <param name="tableName">Nombre de la tabla</param>
+        /// <returns></returns>
+        private bool TableExists(string tableName)
+        {
+            TableMapping map = new TableMapping(typeof(SqlDbType));
+
+            object[] ps = new object[0];
+
+            int tableCount =
+                db.Query(map, "select * from sqlite_master where type = 'table' and name = '" + tableName + "'", ps).Count;
+
+            if (tableCount == 0)
+                return false;
+            else if (tableCount == 1)
+                return true;
+            else
+                throw new Exception($"Hay mas de una tabla con este nombre {tableName} en la base de datos");
+
+        }
+    }
+}
